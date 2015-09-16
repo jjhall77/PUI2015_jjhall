033ba409b5ae01e33b4f1291a046d60076bd05ee
jjhall77 <john.joseph.hall@gmail.com>
2015-09-15 23:07:03 -0400

    ...

--- README.md
+++ README.md
@@ -1,1 +1,6 @@
 # PUI2015_jjhall
+
+Here i describe what i did to set up my environment and how that is helpful.
+
+![Alt text](profile_shot.png)
+![Alt text](commands_shot.png)
---
+++ find_median.py
@@ -0,0 +1,17 @@
+import sys
+
+def hi():
+	print('hello world')
+
+
+if __name__ == '__main__':
+	myList=[]
+	for fileName in sys.argv[1:]:
+
+		
+		inputFile =  open(fileName, 'r')
+		
+		for line in inputFile:
+			myList.append(int(line.strip()))
+	myList.sort()
+	print myList[len(myList)/2]
